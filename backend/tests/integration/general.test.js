ac6fd71b88ebb0ca29eb762f10d341637138e500
ivan-molnar1 <85674932+ivan-molnar1@users.noreply.github.com>
2023-10-24 09:19:49 +0200

    feat: remove backend part

--- backend/tests/integration/general.test.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest');
-const app = require('../../src/server');
-
-describe('GET /v1/not-found', () => {
-  it('should return 404', () => request(app)
-    .get('/v1/not-found')
-    .expect(404));
-});
